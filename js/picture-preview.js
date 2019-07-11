7a8d2b6f908a89ee2b23133df9cf390d43e9e19e
Damal555 <damal@mail.ru>
2019-07-12 02:07:14 +0400

    task 19

--- js/picture-preview.js
+++ js/picture-preview.js
@@ -2,18 +2,21 @@
 (function () {
   window.picturePreview = {
     init: function (data) {
-      picture.classList.remove('hidden');
-      fillPicture(data);
+      addListenersToAllImages(data);
+      addCloseListeners();
     }
   };
+
   var picture = document.querySelector('.big-picture');
 
-  function fillPicture(serverData) {
-    var serverData = serverData[0];
-    var img = picture.querySelector('.big-picture__img');
-    var likes = picture.querySelector('.likes-count');
-    var comments = picture.querySelector('.comments-count');
-    var description = picture.querySelector('.social__caption');
+  var img = picture.querySelector('.big-picture__img img');
+  var likes = picture.querySelector('.likes-count');
+  var comments = picture.querySelector('.comments-count');
+  var description = picture.querySelector('.social__caption');
+
+
+  function fillPicture(data, index) {
+    var serverData = data[index];
     img.src = serverData.url;
     likes.textContent = serverData.likes;
     comments.textContent = serverData.comments.length;
@@ -35,6 +38,37 @@
     });
     target.innerHTML = '';
     target.appendChild(fragment);
+  }
+
+  function addCloseListeners() {
+    var close = picture.querySelector('#picture-cancel');
+    close.addEventListener('click', closePicture);
+    document.addEventListener('keydown', escClose);
+  }
+
+  function closePicture() {
+    picture.classList.add('hidden');
+  }
+
+  function openPicture() {
+    picture.classList.remove('hidden');
+  }
+
+  function escClose(evt) {
+    if (evt.keyCode === 27) {
+      closePicture();
+    }
+  }
+
+  function addListenersToAllImages(data) {
+    var images = window.mock.picturesList.querySelectorAll('.picture');
+    images.forEach(function (elem, index) {
+      elem.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        fillPicture(data, index);
+        openPicture();
+      });
+    });
   }
 
 })();
